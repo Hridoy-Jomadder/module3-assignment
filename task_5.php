e96dc3c80ba6c46b02d3c93e3f4e510151f12a1d
Hridoy Jomadder <79590693+Hridoy-Jomadder@users.noreply.github.com>
2023-09-27 20:10:17 +0600

    Update task_5.php

--- task_5.php
+++ task_5.php
@@ -1,1 +1,15 @@
+<?php
 
+function generatePassword($length)
+{
+    $data = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+";
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomIndex = random_int(0, strlen($data) - 1);
+        $password .= $data[$randomIndex];
+    }
+
+    return $password;
+}
+
+echo generatePassword(12);
