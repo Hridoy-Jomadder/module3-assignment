ec242429d97e6e81cb14babc4a2b239b2bc016de
Hridoy Jomadder <79590693+Hridoy-Jomadder@users.noreply.github.com>
2023-09-27 20:08:42 +0600

    Update task_1.php

--- task_1.php
+++ task_1.php
@@ -1,1 +1,12 @@
+<?php
 
+$text = "The quick brown fox jumps over the lazy dog.";
+
+function modification($input)
+{
+    $lowercase = strtolower($input);
+    $replaced = str_replace("brown", "red", $lowercase);
+    echo "Modified text : {$replaced}";
+}
+
+echo modification($text);
