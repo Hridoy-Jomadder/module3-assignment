85e2fbcb6ebd759f4d3e9367941548597bca7948
Hridoy Jomadder <79590693+Hridoy-Jomadder@users.noreply.github.com>
2023-09-27 20:09:13 +0600

    Update task_2.php

--- task_2.php
+++ task_2.php
@@ -1,1 +1,17 @@
+<?php
 
+$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function removingEvenNumber($n)
+{
+    $result = [];
+    foreach ($n as $number) {
+        if ($number % 2 != 0) {
+            $result[] = $number;
+        }
+    }
+
+    print_r($result);
+}
+
+removingEvenNumber($numbers);
